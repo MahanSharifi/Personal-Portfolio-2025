1667c9edc87d059ad957a03d4aa10a216e895b0d
MahanSharifii <mahan9879@gmail.com>
2025-03-20 22:49:44 -0400

    Fix image optimization for static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  basePath: isGithubPages ? '/personal-port' : '',
+  assetPrefix: isGithubPages ? '/personal-port/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
